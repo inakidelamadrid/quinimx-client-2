fa98b59cf3ed6fe7801d4ac039d319cac01a458b
Ignacio De La Madrid <ignacio.delamadrid@gmail.com>
2019-10-14 10:54:30 -0500

    Install node-sass. Install bulma-react-components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
+//@format
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import 'react-bulma-components/dist/react-bulma-components.min.css';
+import {Container, Section, Hero, Heading} from 'react-bulma-components';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="QuinielaUI">
+        <div className="App-body">
+          <Container>
+            <Section>
+              <Hero color="primary">
+                <Hero.Body>
+                  <Container>
+                    <Heading>Quiniela Acumulativa Liga MX</Heading>
+                    <Heading subtitle size={3}>
+                      Jornada 5
+                    </Heading>
+                  </Container>
+                </Hero.Body>
+              </Hero>
+            </Section>
+            <Section></Section>
+          </Container>
+        </div>
+      </div>
     </div>
   );
 }
