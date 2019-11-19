de5900a48a743683476511d76f76f037fa6afb9f
Ignacio De La Madrid <ignacio.delamadrid@gmail.com>
2019-11-19 11:31:31 -0600

    Setup react router paths

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 //@format
-import React from 'react';
-import logo from './logo.svg';
-import './App.scss';
-import 'react-bulma-components/dist/react-bulma-components.min.css';
-import {Container, Section} from 'react-bulma-components';
-import Header from './components/Header';
+import React from 'react'
+import { Route, Switch } from 'react-router'
+import './App.scss'
+import 'react-bulma-components/dist/react-bulma-components.min.css'
+import { Container, Section } from 'react-bulma-components'
+import Header from './components/Header'
 
 function App() {
   return (
@@ -12,16 +12,24 @@ function App() {
       <div className="QuinielaUI">
         <div className="App-body">
           <Container>
-            <Header
-              header="Quiniela Acumulativa Liga MX"
-              subheader="Jornada 5"
-            />
-            <Section></Section>
+            <Switch>
+              <Route exact path="/">
+                <Header
+                  header="Quiniela Acumulativa Liga MX"
+                  subheader="Jornada 5"
+                />
+                <Section></Section>
+              </Route>
+              <Route path="/form">
+                <Header header="Formulario" subheader="Aqui se meteran datos" />
+                <Section></Section>
+              </Route>
+            </Switch>
           </Container>
         </div>
       </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom'
 import './index.scss'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
+import { BrowserRouter as Router } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import { createStore, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
@@ -11,7 +12,9 @@ import rootReducer from './reducers'
 const store = createStore(rootReducer, applyMiddleware(thunk))
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </Provider>,
   document.getElementById('root')
 )
