2cc070a1a535a32b7365ed9206e3d3febb197c8a
Ignacio De La Madrid <ignacio.delamadrid@gmail.com>
2019-11-20 13:33:34 -0600

    Remove all occurences of bulma within the project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
 //@format
 import React from 'react'
 import { Route, Switch } from 'react-router'
-import './App.scss'
-import 'react-bulma-components/dist/react-bulma-components.min.css'
-import { Container, Section } from 'react-bulma-components'
-import Header from './components/Header'
+import './styles/globals.scss'
 import CurrentMatchWeekPage from './pages/CurrentMatchWeekPage'
 import MatchWeekFormPage from './pages/MatchWeekFormPage'
 
@@ -13,23 +10,17 @@ function App() {
     <div className="App">
       <div className="QuinielaUI">
         <div className="App-body">
-          <Container>
-            <Switch>
-              <Route exact path="/">
-                <Header
-                  header="Quiniela Acumulativa Liga MX"
-                  subheader="Jornada 5"
-                />
-                <Section></Section>
-              </Route>
-              <Route path="/form">
-                <MatchWeekFormPage />
-              </Route>
-              <Route path="/quiniela/jornada/actual">
-                <CurrentMatchWeekPage />
-              </Route>
-            </Switch>
-          </Container>
+          <Switch>
+            <Route exact path="/">
+              <div>root</div>
+            </Route>
+            <Route path="/form">
+              <MatchWeekFormPage />
+            </Route>
+            <Route path="/quiniela/jornada/actual">
+              <CurrentMatchWeekPage />
+            </Route>
+          </Switch>
         </div>
       </div>
     </div>
--- src/components/Header/header.story.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import { storiesOf } from '@storybook/react'
-
-
-import Header from './index'
-
-storiesOf('Header', module).add('typical', () => <Header header="Hello" subheader="This is a test"/>)
