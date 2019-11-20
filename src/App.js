eeb701bb74b627cb317a9926ba3bba86fdcb812b
Ignacio De La Madrid <ignacio.delamadrid@gmail.com>
2019-11-20 13:39:28 -0600

    created a responsive container component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import React from 'react'
 import { Route, Switch } from 'react-router'
 import './styles/globals.scss'
+import Container from './components/Container'
 import CurrentMatchWeekPage from './pages/CurrentMatchWeekPage'
 import MatchWeekFormPage from './pages/MatchWeekFormPage'
 
@@ -10,17 +11,19 @@ function App() {
     <div className="App">
       <div className="QuinielaUI">
         <div className="App-body">
-          <Switch>
-            <Route exact path="/">
-              <div>root</div>
-            </Route>
-            <Route path="/form">
-              <MatchWeekFormPage />
-            </Route>
-            <Route path="/quiniela/jornada/actual">
-              <CurrentMatchWeekPage />
-            </Route>
-          </Switch>
+          <Container>
+            <Switch>
+              <Route exact path="/">
+                <div>root</div>
+              </Route>
+              <Route path="/form">
+                <MatchWeekFormPage />
+              </Route>
+              <Route path="/quiniela/jornada/actual">
+                <CurrentMatchWeekPage />
+              </Route>
+            </Switch>
+          </Container>
         </div>
       </div>
     </div>
